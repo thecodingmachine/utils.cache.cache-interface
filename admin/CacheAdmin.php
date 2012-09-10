05ddedf6998b40c764eba1f9e75c739e823c2fa3
David Négrier <david@davidtcm>
2012-09-10 19:04:49 +0200

    Deleting unused files

--- admin/CacheAdmin.php
+++
@@ -1,8 +0,0 @@
-<?php
-MoufUtils::registerMainMenu('utilsMainMenu', 'Utils', null, 'mainMenu', 200);
-MoufUtils::registerMenuItem('utilsCacheInterfaceMenu', 'Cache management', null, 'utilsMainMenu', 50);
-MoufUtils::registerMenuItem('utilsCacheInterfacePurgeAllCachesMenuItem', 'Purge all caches', 'mouf/purgeCaches/', 'utilsCacheInterfaceMenu', 10);
-
-// Controller declaration
-MoufManager::getMoufManager()->declareComponent('purgeCaches', 'PurgeCacheController', true);
-MoufManager::getMoufManager()->bindComponents('purgeCaches', 'template', 'moufTemplate');
