f35bf544b223bd52e94c92cc42291acff602a357
David Négrier <david@davidtcm>
2012-11-03 16:55:22 +0100

    Adding purge buttons

--- CacheAdmin.php
+++ CacheAdmin.php
@@ -1,8 +1,35 @@
 <?php
+
+use Mouf\Html\HtmlElement\HtmlFromFile;
+
+use Mouf\Html\HtmlElement\HtmlString;
+
+use Mouf\MoufManager;
+
+use Mouf\MoufUtils;
+
+// Because cache-interface can be used in both the application and in Mouf admin, there are 2
+// PurgeCacheController classes. We must be sure to load the right one.
+// So instead of relying on the autoloader that will favor the class in Mouf admin, we force the
+// loading of the class.
+require_once 'src/Mouf/Utils/Cache/Admin/Controllers/PurgeCacheController.php';
+
 MoufUtils::registerMainMenu('utilsMainMenu', 'Utils', null, 'mainMenu', 200);
 MoufUtils::registerMenuItem('utilsCacheInterfaceMenu', 'Cache management', null, 'utilsMainMenu', 50);
-MoufUtils::registerMenuItem('utilsCacheInterfacePurgeAllCachesMenuItem', 'Purge all caches', 'mouf/purgeCaches/', 'utilsCacheInterfaceMenu', 10);
+MoufUtils::registerMenuItem('utilsCacheInterfacePurgeAllCachesMenuItem', 'Purge all caches', 'purgeCaches/', 'utilsCacheInterfaceMenu', 10);
+
+$moufManager = MoufManager::getMoufManager();
+$navbar = $moufManager->getInstance("navBar");
+/*$htmlString = new HtmlString();
+$htmlString->htmlString = '<form class="navbar-form pull-right" style="margin-right: 5px">
+    <button class="btn btn-danger">Purge cache</button>
+    </form>';*/
+$navbar->children[] = new HtmlFromFile("../../../vendor/mouf/utils.cache.cache-interface/src/views/purgebutton.php");
 
 // Controller declaration
-MoufManager::getMoufManager()->declareComponent('purgeCaches', 'PurgeCacheController', true);
-MoufManager::getMoufManager()->bindComponents('purgeCaches', 'template', 'moufTemplate');
+$moufManager->declareComponent('purgeCaches', 'Mouf\\Utils\\Cache\\Admin\\Controllers\\PurgeCacheController', true);
+$moufManager->bindComponents('purgeCaches', 'template', 'moufTemplate');
+$moufManager->bindComponents('purgeCaches', 'content', 'block.content');
+
+
+unset($moufManager);
--- src/direct/purge_all.php
+++ src/direct/purge_all.php
@@ -1,21 +1,33 @@
 <?php
+use Mouf\MoufManager;
+
 // This file purges all the caches of any instance implementing the CacheInterface interface.
 
+// Disable output buffering
+while (ob_get_level() != 0) {
+	ob_end_clean();
+}
+
+ini_set('display_errors', 1);
+// Add E_ERROR to error reporting if it is not already set
+error_reporting(E_ERROR | error_reporting());
+
 if (!isset($_REQUEST["selfedit"]) || $_REQUEST["selfedit"]!="true") {
-	require_once '../../../../../../../Mouf.php';
+	require_once '../../../../../mouf/Mouf.php';
+	$mouf_base_path = ROOT_PATH;
+	$selfEdit = false;
 } else {
-	require_once '../../../../../../../mouf/MoufManager.php';
-	MoufManager::initMoufManager();
-	require_once '../../../../../../../MoufUniversalParameters.php';
-	require_once '../../../../../../../mouf/MoufAdmin.php';
+	require_once '../../mouf/Mouf.php';
+	$mouf_base_path = ROOT_PATH."mouf/";
+	$selfEdit = true;
 }
 
 // Note: checking rights is done after loading the required files because we need to open the session
 // and only after can we check if it was not loaded before loading it ourselves...
-require_once '../../../../../../../mouf/direct/utils/check_rights.php';
+require_once '../../../../../vendor/mouf/mouf/src/direct/utils/check_rights.php';
 
 $moufManager = MoufManager::getMoufManager();
-$instances = $moufManager->findInstances("CacheInterface");
+$instances = $moufManager->findInstances("Mouf\\Utils\\Cache\\CacheInterface");
 
 foreach ($instances as $instanceName) {
 	$cacheService = $moufManager->getInstance($instanceName);
